1244154f859e8a9b0bb0f75b2579469ff76e9325
cwolf <christof@aon.at>
2013-07-01 23:00:54 +0200

    Revert "v0.1"
    This reverts commit a3bf5588a76ee359d359c4e944caf62bbf2db709.

--- php/delDetails.php
+++ php/delDetails.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 if (isset($_GET['id']) && isset($_SESSION['Admin']) && $_SESSION['Admin'] === "true") {
     include 'mysql.php';
     mysql_query('delete from Details where ID = "'  .$_GET['id'] . '" ');
