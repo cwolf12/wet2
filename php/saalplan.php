56a86d33db502a434dfeea00d8cfda4802a914f8
cwolf <cwolf@cwolf-laptop>
2013-07-01 22:49:09 +0200

    v0.1

---
+++ php/saalplan.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of saalplan
+ *
+ * @author cwolf
+ */
+class saalplan {
+    //put your code here
+}
+
+?>
