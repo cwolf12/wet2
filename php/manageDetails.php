106387fb02ba7e44abc4acc200259dd416b3a198
cwolf <cwolf@cwolf-laptop>
2013-07-02 22:35:09 +0200

    kleinere Anpassungen - Christof

--- php/manageDetails.php
+++ php/manageDetails.php
@@ -6,14 +6,14 @@ if (isset($_SESSION['Admin']) && $_SESSION['Admin'] === "true") {
     if (isset($_POST['delete']) && isset($_POST['id'])) {
         $sql = "delete from Details where ID = '" . $_POST['id'] . "'";
     }
-    if (isset($_POST['titel']) && isset($_POST['info']) && isset($_POST['foto'])
+    elseif (isset($_POST['titel']) && isset($_POST['info']) && isset($_POST['foto'])
             && isset($_POST['bewertung'])) {
         if (isset($_POST['update']) && isset($_POST['id'])) {
             $sql = "update Details set Titel = '" . $_POST['titel'] . "',
                     Info='" . $_POST['info'] . "', Foto = '" . $_POST['foto'] . "',
                         Bewertung= '" . $_POST['bewertung'] . "' where ID = '" . $_POST['id'] . "'";
         }
-        if (isset($_POST['speichern'])) {
+        elseif (isset($_POST['speichern'])) {
             $sql = "insert into Details (Titel, Info, Foto, Bewertung) values 
                 ('" . $_POST['titel'] . "','" . $_POST['info'] . "','" . $_POST['foto'] . "',
                     '" . $_POST['bewertung'] . "')";
--- php/manageProgramm.php
+++ php/manageProgramm.php
@@ -6,14 +6,14 @@ if (isset($_SESSION['Admin']) && $_SESSION['Admin'] === "true") {
     if (isset($_POST['delete']) && isset($_POST['id'])) {
         $sql = "delete from Programm where ID = '" . $_POST['id'] . "'";
     }
-    if (isset($_POST['datum']) && isset($_POST['uhrzeit']) && isset($_POST['titel'])
+    elseif (isset($_POST['datum']) && isset($_POST['uhrzeit']) && isset($_POST['titel'])
             && isset($_POST['art'])) {
         if (isset($_POST['update']) && isset($_POST['id'])) {
             $sql = "update Programm set Datum = '" . $_POST['datum'] . "',
                     Uhrzeit='" . $_POST['uhrzeit'] . "', Titel = '" . $_POST['titel'] . "',
                         Art= '" . $_POST['art'] . "' where ID = '" . $_POST['id'] . "'";
         }
-        if (isset($_POST['speichern'])) {
+        elseif (isset($_POST['speichern'])) {
             $sql = "insert into Programm (Datum, Uhrzeit, Titel, Art) values 
                 ('" . $_POST['datum'] . "','" . $_POST['uhrzeit'] . "','" . $_POST['titel'] . "',
                     '" . $_POST['art'] . "')";
